c604e2e53a7bf6e797bcdfcb4e6e2a8a39cc3951
daumtto53 <daumtto53@gmail.com>
2024-04-14 17:26:04 +0900

    feat(front): add board-reply componenet & style

--- frontend/src/BoardRead/BoardReply.jsx
+++ frontend/src/BoardRead/BoardReply.jsx
@@ -1,18 +1,39 @@
 import styles from "./css/BoardRead.module.css";
+import { IoPerson } from "react-icons/io5";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 function ReplyForm() {
     return (
-        <form className={styles["reply-register-outer-wrapper"]}>
-            <div className={styles["reply-register-inner-wrapper"]}>
-                <div className={styles["register-id"]}>
-                    <input type="text" /> ID
+        <form className={styles["reply-form-outer-wrapper"]}>
+            <div className={styles["reply-form-inner-wrapper"]}>
+                <div className={styles["id-cell"]}>
+                    <input disabled className={styles["reply-form-id-box"]} type="text" />
                 </div>
-                <div className={styles["register-textfield"]}>
-                    <input type="text" />
+                <div className={styles["textarea-cell"]}>
+					<TextField
+						name="reply-content"
+						multiline={true}
+						rows={3}
+						sx={{
+							width:"100%",
+							height:"100%",
+							justifyContent:"stretch"
+						} }
+					/>
+
+					{/*<textarea className={styles["reply-form-textarea"]}/>*/}
+
                     {/*<input type='text' className={styles.test} /> TextField*/}
                 </div>
             </div>
-            <button>post comment</button>
+				 <Button
+				 	type="submit"
+				 	variant="contained"
+					sx={{
+						marginTop:"2rem"
+					}}
+				 >Post Reply</Button>
         </form>
     );
 }
@@ -20,16 +41,29 @@ function ReplyForm() {
 function ReplyBox() {
     return (
         <div className={styles.replybox}>
-            <div className={styles["profile-icon"]}></div>
-            <div className={styles.id}>
-                <input type="text" />
+            <div className={styles["profile-icon"]}>
+                <IoPerson className={styles.icon} />
             </div>
-			<div className={styles.timestamp}>
-				{/*<input type="text" />*/}
-				asdf
-			</div>
+            <div className={styles.id}>ID</div>
+            <div className={styles.timestamp}>Time</div>
             <div className={styles.reply}>
-                <input type="textarea" />
+				{/* duplicatte element id */}
+                <TextField
+					id="reply-textfield"
+					variant="outlined"
+					disabled={true}
+					multiline={true}
+					rows={3}
+					value="12313
+					123
+					123"
+					sx={{
+						overflowY: "auto",
+						width:"100%",
+						height:"100%",
+						padding:"1rem",
+					}}
+				/>
             </div>
         </div>
     );
@@ -39,13 +73,13 @@ export default function BoardReply() {
     return (
         <div className={styles["reply-wrapper"]}>
             <div className={styles["reply-list"]}>
-				<ReplyBox />
-				<ReplyBox />
-				<ReplyBox />
-				<ReplyBox />
-				<ReplyBox />
-				<ReplyBox />
-			</div>
+                <ReplyBox />
+                <ReplyBox />
+                <ReplyBox />
+                <ReplyBox />
+                <ReplyBox />
+                <ReplyBox />
+            </div>
 
             <ReplyForm />
         </div>
