d7434b46f91def5bd5b8ea21e8212dc3cac4ae32
daumtto53 <daumtto53@gmail.com>
2024-04-16 20:11:08 +0900

    style(front): unifying styling with board width.

--- frontend/src/BoardWrite/BoardWrite.jsx
+++ frontend/src/BoardWrite/BoardWrite.jsx
@@ -5,9 +5,6 @@ import styles from "./css/BoardWrite.module.css";
 export default function BoardWrite() {
     return (
         <div className={styles["board-write-container"]}>
-            <header>
-				<h1>Write on Board</h1>
-			</header>
             <main>
 				<section>
 					<BoardWriteForm />
