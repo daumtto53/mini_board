e41b7a70e553d4900295502a0d86269d7508d4f9
daumtto53 <daumtto53@gmail.com>
2024-05-01 15:09:20 +0900

    feat(modifyboard): modifying board now works.

--- frontend/src/BoardWrite/BoardWrite.jsx
+++ frontend/src/BoardWrite/BoardWrite.jsx
@@ -1,25 +1,27 @@
+import { useLocation } from "react-router-dom";
 import BoardForm from "../BoardCommonComponents/BoardForm";
+import { useEffect, useState } from "react";
 
 import styles from "./css/BoardWrite.module.css";
+import { pageAxios } from "../API/boardAPI";
 
 /**
  * boardData
  * 		author content title updatedAt views
  */
+
 export default function BoardWrite() {
-    const boardData = {
-        author: "",
-        content: "",
-        title: "",
-        updatedAt: "",
-        views: "",
-    };
+    const loc = useLocation();
+	const postId = loc.state === null ? null : loc.state.postId;
+	const mode = loc.state === null ? "write" : "modify"
+
     return (
         <div className={styles["board-write-wrapper"]}>
             <div className={styles["board-write-container"]}>
                 <main>
                     <section>
-                        <BoardForm boardData={boardData} mode={"write"} />
+                        <BoardForm postId={postId} mode={mode} />
+						<></>
                     </section>
                 </main>
             </div>
