ed43af9adb8c0661c4f4ac5912b6317f09ba2794
daumtto53 <daumtto53@gmail.com>
2024-07-09 18:30:12 +0900

    refactor(cors): configurated s3-endpoint in applications.properties for expansion.

--- backend/src/main/java/com/cms/mini_board/config/cors/CorsConfig.java
+++ backend/src/main/java/com/cms/mini_board/config/cors/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.cms.mini_board.config.cors;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,13 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Log4j2
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${s3-endpoint}")
+    private String endpoint;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        String uri = String.format("http://%s:5173", endpoint);
+        String uri = String.format("http://%s", endpoint);
+        log.info(uri);
+
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
 //                .allowedOrigins("http://127.0.0.1:5173")
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
+                .allowedOrigins(uri)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
@@ -21,7 +32,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
+                .allowedOrigins(uri)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
